54e3f9d979ac6fc965c7708743368f064e4b7def
Diogo Lima <diogo@dr6.com.br>
2021-06-12 21:56:55 +0000

    fixes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import uvicorn
 import pydantic
 from fastapi import FastAPI, exceptions
+from starlette.middleware.cors import CORSMiddleware
 import mongoengine
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 mongoengine.connect('store')
 
 
