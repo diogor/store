1e54a2660af709360ac328ccfcd4b11477a60d23
Diogo Lima <diogo@dr6.com.br>
2021-08-20 12:59:16 +0000

    bug de apagar

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def insert_record(data) -> str:
 def clean_db():
     time_period = datetime.now() - timedelta(hours=24)
     try:
-        records = RecordDocument.objects.get(created_at__lt=time_period)
+        records = RecordDocument.objects(created_at__lt=time_period)
         records.delete()
     except mongoengine.DoesNotExist:
         pass
